afe000b8d3db15f3278cad070173d3fcdb65d9d0
Vicky <guptavicky1009@gmail.com>
2026-05-31 14:38:40 +0530

    chnages for knowing some function

--- src/androidTest/java/com/example/bmi/ExampleInstrumentedTest.java
+++ src/androidTest/java/com/example/bmi/ExampleInstrumentedTest.java
@@ -9,7 +9,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
-
+// junit import for what reason?
 /**
  * Instrumented test, which will execute on an Android device.
  *
